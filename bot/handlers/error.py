b99cf280a01e5fe2fc28e16f24ede87e79f3ffbc
dn1lk <sh.dn1lk@gmail.com>
2022-06-20 18:40:41 +0300

    GOL'D ADD

--- bot/handlers/error.py
+++ bot/handlers/error.py
@@ -6,17 +6,11 @@ from aiogram import Router, Bot, types, exceptions
 router = Router(name='error')
 
 
-@router.errors(pattern="message is not modified")
-async def edit_handler(_):
-    pass
-
-
 @router.errors(exception=exceptions.TelegramRetryAfter)
-async def retry_after_handler(event: types.Update, bot: Bot, exception: exceptions.TelegramRetryAfter):
+async def retry_after_handler(_, exception: exceptions.TelegramRetryAfter):
     logging.error(f'TelegramRetryAfter: sleeping for {exception.retry_after} seconds')
 
     await asyncio.sleep(exception.retry_after)
-    await bot.send_message(bot.owner_id, f'FLOOD while event <b>{event.event_type}</b>')
     await exception.method
 
 
@@ -31,6 +25,6 @@ async def errors_handler(event: types.Update, bot: Bot, exception: Exception):
             )
         )
     except exceptions.TelegramBadRequest:
-        logging.critical("TelegramBadRequest: Can't send error message")
+        logging.critical("TelegramBadRequest: can't send error message")
     finally:
         logging.exception(exception)
