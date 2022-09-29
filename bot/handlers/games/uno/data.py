4a4124b22639e8ea43f5cb7fadedb493a7a2057d
dn1lk <sh.dn1lk@gmail.com>
2022-09-29 22:37:57 +0300

    update uno

--- bot/handlers/games/uno/data.py
+++ bot/handlers/games/uno/data.py
@@ -27,7 +27,7 @@ class UnoData(BaseModel):
     current_skip: int | bool = False
 
     polls_kick: dict[str, UnoPollKick] = {}
-    queries: set[int] = {}
+    queries: set[int] = set()
     timer_amount: int = 3
 
     @property
