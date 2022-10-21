29a2eb76b002893560afe31ddc9f163515742b28
dn1lk <96176828+dn1lk@users.noreply.github.com>
2022-10-21 11:16:02 +0300

    Update settings.py

--- bot/handlers/games/uno/settings.py
+++ bot/handlers/games/uno/settings.py
@@ -8,9 +8,13 @@ from ... import get_username
 router = Router(name='game:uno:settings')
 
 
-def get_current_difficulty(message: types.Message):
-    difficulty = message.entities[1]
-    return message.text[difficulty.offset:difficulty.offset + difficulty.length]
+def get_current_difficulty(message: types.Message) -> UnoDifficulty:
+    difficulty_name = message.entities[1]
+    difficulty_name = message.text[difficulty.offset:difficulty.offset + difficulty.length]
+    
+    for difficulty in UnoDifficulty:
+        if difficulty.name == difficulty_name:
+            return difficulty
 
 
 @router.callback_query(k.Games.filter(F.value == 'difficulty'), F.from_user.id == F.message.entities[3].user.id)
@@ -18,21 +22,18 @@ async def difficulty_handler(query: types.CallbackQuery):
     await query.message.edit_reply_markup(k.uno_difficulties(get_current_difficulty(query.message)))
 
 
-@router.callback_query(
-    k.Games.filter(F.value.in_(tuple(difficulty.value for difficulty in UnoDifficulty))),
-    F.from_user.id == F.message.entities[3].user.id,
-)
+@router.callback_query(F.from_user.id == F.message.entities[3].user.id, k.Games.filter(F.value.in_(UnoDifficulty)))
 async def difficulty_change_handler(query: types.CallbackQuery, callback_data: k.Games):
     await query.message.edit_text(
         query.message.html_text.replace(
-            get_current_difficulty(query.message),
-            callback_data.value,
+            get_current_difficulty(query.message).name,
+            callback_data.name,
         ),
         reply_markup=k.uno_start(),
     )
 
 
-@router.callback_query(k.Games.filter(F.value == 'back'), F.from_user.id == F.message.entities[3].user.id)
+@router.callback_query(F.from_user.id == F.message.entities[3].user.id, k.Games.filter(F.value == 'back'))
 async def difficulty_back_handler(query: types.CallbackQuery):
     await query.message.edit_reply_markup(k.uno_start())
 
