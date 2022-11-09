3e40438e51dd45f08aa71694df6cc307a4d16a84
dn1lk <96176828+dn1lk@users.noreply.github.com>
2022-11-09 15:15:05 +0300

    Update process.py

--- bot/handlers/settings/commands/process.py
+++ bot/handlers/settings/commands/process.py
@@ -15,7 +15,7 @@ router = Router(name='settings:commands:process')
 
 
 async def process_filter(message: types.Message, bot: Bot, commands: dict[str, types.BotCommand]):
-    return await filters.Command(*commands['en'])(message, bot)
+    return await filters.Command(commands['en'])(message, bot)
 
 
 router.message.filter(Settings.command, process_filter, f.AdminFilter(is_admin=True))
