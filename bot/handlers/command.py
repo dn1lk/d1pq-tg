3a85b456fd3ceaed79f6c8de5c0039aec617c8b3
dn1lk <sh.dn1lk@gmail.com>
2022-11-13 16:59:17 +0300

    bugfixes

--- bot/handlers/command.py
+++ bot/handlers/command.py
@@ -148,13 +148,28 @@ async def game_handler(message: types.Message):
 
 
 @router.message(CustomCommandFilter('play', 'поиграем'))
-async def game_no_args_handler(message: types.Message):
-    await message.answer(
-        _(
-            "<b>And what do you want to play?</b>\n\n"
-            "Try to guess the game by writing the right words right after the command."
+async def game_no_args_handler(message: types.Message, command: filters.CommandObject):
+    def get_bot_args(r: Router):
+        for f in r.message._handler.filters:
+            if isinstance(f.callback, CustomCommandFilter):
+                yield f.callback.magic._operations[1].args[0][0]
+
+        for r in r.sub_routers:
+            if r:
+                yield from get_bot_args(r)
+
+    answer = html.bold(_("<b>And what do you want to play?</b>"))
+
+    if random() > 0.5:
+        answer += _("\n\nTry to guess the game by writing the right words right after the command.")
+    else:
+        args = list(get_bot_args(router.parent_router))
+        answer += NO_ARGS.format(
+            command=command.command,
+            args=choice(args),
         )
-    )
+
+    await message.answer(answer)
 
 
 @router.message(CustomCommandFilter('question', 'вопросик', magic=F.args))
